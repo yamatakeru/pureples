4dc959f723922dc1689d022974b2cb5e15eae491
yamatakeru <yamato_y@me.com>
2022-02-17 20:53:14 +0900

    Option names changing.

--- pureples/shared/genes.py
+++ pureples/shared/genes.py
@@ -3,10 +3,10 @@ from neat.genes import BaseGene
 
 
 class OutputNodeGene(BaseGene):
-    _gene_attributes = [FloatAttribute('obias'),
-                        FloatAttribute('oresponse'),
-                        StringAttribute('oactivation', options='sigmoid'),
-                        StringAttribute('oaggregation', options='sum')]
+    _gene_attributes = [FloatAttribute('output_bias'),
+                        FloatAttribute('output_response'),
+                        StringAttribute('output_activation', options='sigmoid'),
+                        StringAttribute('output_aggregation', options='sum')]
 
     def __init__(self, key):
         assert isinstance(key, int), "OutputNodeGene key must be an int, not {!r}".format(key)
@@ -14,40 +14,41 @@ class OutputNodeGene(BaseGene):
 
     @property
     def bias(self):
-        return self.obias
+        return self.output_bias
 
     @bias.setter
-    def bias(self, obias):
-        self.obias = obias
+    def bias(self, output_bias):
+        self.output_bias = output_bias
 
     @property
     def response(self):
-        return self.oresponse
+        return self.output_response
 
     @response.setter
-    def response(self, oresponse):
-        self.oresponse = oresponse
+    def response(self, output_response):
+        self.output_response = output_response
 
     @property
     def activation(self):
-        return self.oactivation
+        return self.output_activation
 
     @activation.setter
-    def activation(self, oactivation):
-        self.oactivation = oactivation
+    def activation(self, output_activation):
+        self.output_activation = output_activation
 
     @property
     def aggregation(self):
-        return self.oaggregation
+        return self.output_aggregation
 
     @aggregation.setter
-    def aggregation(self, oaggregation):
-        self.oaggregation = oaggregation
+    def aggregation(self, output_aggregation):
+        self.output_aggregation = output_aggregation
 
     def distance(self, other, config):
-        d = abs(self.obias - other.obias) + abs(self.oresponse - other.oresponse)
-        if self.oactivation != other.oactivation:
+        d = abs(self.output_bias - other.output_bias) + abs(self.output_response - other.output_response)
+        if self.output_activation != other.output_activation:
             d += 1.0
-        if self.oaggregation != other.oaggregation:
+        if self.output_aggregation != other.output_aggregation:
             d += 1.0
+
         return d * config.compatibility_weight_coefficient
